3659e54675a24645dfb76cc7eea70558aba31b10
harclemadscam <joeapplegate1993@gmail.com>
2020-09-07 14:42:16 +0000

    Removed productimage model and added extra image fields to product

--- shop/models.py
+++ shop/models.py
@@ -21,7 +21,10 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, related_name='products', on_delete=models.SET_NULL)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    primary_image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image1 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image2 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image3 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image4 = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -34,17 +37,6 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
-
-    def __str__(self):
-        return self.name
-
-
-class ProductImage(models.Model):
-    product = models.ForeignKey('Product', related_name='product_image', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/%Y/%m/%d')
-
-    class Meta:
-        ordering = ('product',)
 
     def __str__(self):
         return self.name
