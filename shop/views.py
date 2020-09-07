eaf7c61bdd4c9618b8876fd6db5d21f3816c0ef9
harclemadscam <joeapplegate1993@gmail.com>
2020-09-07 17:19:35 +0000

    Updated shop view to include category from models

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 
 def shop(request):
     """ Returns the shop products """
+    categories = Category.objects.all()
     products = Product.objects.all()
-
     context = {
-        'products' : products,
+        'products': products,
+        'categories': categories,
     }
 
     return render(request, 'shop/products.html', context)
