03e5da48878676f86a184c0e384d079434532d3e
harclemadscam <joeapplegate1993@gmail.com>
2020-09-07 15:19:01 +0000

    Added shop view to shop view.py to render products

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def shop(request):
+    """ Returns the shop products """
+    products = Product.objects.all()
+
+    context = {
+        'products' : products,
+    }
+
+    return render(request, 'shop/products.html', context)
