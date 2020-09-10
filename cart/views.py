b827da612aa835519763350ad355355b2a13f132
harclemadscam <joeapplegate1993@gmail.com>
2020-09-10 19:41:32 +0000

    Created initial add to cart view in cart views.py

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from shop.models import Product, Category
 
 # Create your views here.
@@ -11,3 +11,19 @@ def view_cart(request):
 
     }
     return render(request, 'cart/cart.html', context)
+
+
+def add_to_cart(request, product_id):
+    """ Add product to cart with correct quantity and spec"""
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_path = request.POST.get('redirect_path')
+    cart = request.session.get('cart', {})
+    if product_id in list(cart.keys()):
+        cart[product_id]['quantity'] += quantity
+    else:
+        cart[product_id] = {'quantity': quantity,
+                            'price': str(product.price)}
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_path)
