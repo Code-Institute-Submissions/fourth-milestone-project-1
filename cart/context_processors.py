99696478f4f5d9a913b2102578663babf2950ecf
harclemadscam <joeapplegate1993@gmail.com>
2020-09-11 14:40:30 +0000

    Updated the cart context processor to use the cart class

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,15 +1,4 @@
+from .cart import Cart
 
-
-def cart_info(request):
-
-    cart_items = []
-    total_cost = 0
-    product_count = 0
-
-    context = {
-        'cart_items': cart_items,
-        'total_cost': total_cost,
-        'product_count': product_count,
-    }
-
-    return context
+def cart(request):
+    return {'cart': Cart(request)}
