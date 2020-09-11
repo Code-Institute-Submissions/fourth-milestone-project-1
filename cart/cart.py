54f9e46f8b43247220bcb48a98e6b1d62902c460
harclemadscam <joeapplegate1993@gmail.com>
2020-09-11 13:48:13 +0000

    Added remove from cart and iterate methods to cart class

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from decimal import Decimal
+from shop.models import Product
 
 
 class Cart(object):
@@ -23,4 +25,22 @@ class Cart(object):
 
     def save(self):
         self.session.modified = True
-        
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.save()
+    
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    
