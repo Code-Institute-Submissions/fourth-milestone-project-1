d7e0ca3ed48ab316ad322e46c9576d00c0381dc5
harclemadscam <joeapplegate1993@gmail.com>
2020-09-20 17:27:50 +0000

    Updated orders urls.py to include webhook url

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('create/', views.create_order, name='create_order'),
+    path('wh/', webhook, name='webhook'),
 ]
