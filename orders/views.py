4b77272163ee6f02c2bc37ccfa059fff4660de04
harclemadscam <joeapplegate1993@gmail.com>
2020-09-20 14:49:02 +0000

    Updated create order view to create stripe payment intent

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import CreateOrderForm
 from cart.cart import Cart
+from django.conf import settings
+
+import stripe
 
 
 def create_order(request):
     cart = Cart(request)
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     if request.method == 'POST':
         form = CreateOrderForm(request.POST)
         if form.is_valid():
@@ -19,10 +24,17 @@ def create_order(request):
             return render(request, 'orders/order_complete.html', {'order': order})
     else:
         form = CreateOrderForm()
+        total = cart.get_total_price()
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
         context = {
             'cart': cart,
             'form': form,
-            'stripe_public_key': "pk_test_51HTReHJApFJ5JC2dtbCYysmWfbJicF98ZxPtQ5sFsRLsCkD10WQU7soWW7mBOdx2ttR0IIkYYQFtPLnPN1fz5kTr00ERsZXurp",
-            'client_secret': 'test',
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
         }
     return render(request, 'orders/create_order.html', context)
