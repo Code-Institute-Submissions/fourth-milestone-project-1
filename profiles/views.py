67e533f68d367a7a2d7ee942da280d6842200971
harclemadscam <joeapplegate1993@gmail.com>
2020-09-27 19:18:21 +0000

    Updated the user profile view to render the form and the user's orders

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserOrderDetailsForm
 
 
 def user_profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserOrderDetailsForm(instance=user_profile)
+    orders = user_profile.orders.all()
     context = {
-        'user_profile': user_profile
+        'user_profile': user_profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'profiles/profile.html', context)
