d8dbb678faa413d26fd3bc7bbc5986fc32d451f1
harclemadscam <joeapplegate1993@gmail.com>
2020-09-27 13:25:37 +0000

    Updated create order form with name on card field

--- orders/forms.py
+++ orders/forms.py
@@ -6,4 +6,5 @@ class CreateOrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address_line1',
-                  'address_line2', 'town_or_city', 'postcode', 'country']
+                  'address_line2', 'town_or_city', 'postcode', 'country',
+                  'name_on_card']
