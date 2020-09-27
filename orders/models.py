ef096d943bbfe8b60f4634de047339ddad67212e
harclemadscam <joeapplegate1993@gmail.com>
2020-09-27 13:25:03 +0000

    Updated order model with name on card field, made migrations

--- orders/models.py
+++ orders/models.py
@@ -14,6 +14,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    name_on_card = models.CharField(max_length=100, null=False, blank=False)
 
     class Meta:
         ordering = ('-created',)
