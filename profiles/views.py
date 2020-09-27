ec9c4eaff21fbc881406504c4971fd9cf438a888
harclemadscam <joeapplegate1993@gmail.com>
2020-09-27 16:53:49 +0000

    Created the user profile view for the profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def user_profile(request):
-    context = {}
-    
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        'user_profile': user_profile
+    }
     return render(request, 'profiles/profile.html', context)
