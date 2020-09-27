6af71d85dfdd4bb791b39934caab8c6ab41a9dff
harclemadscam <joeapplegate1993@gmail.com>
2020-09-27 14:35:46 +0000

    Added the user profile foreign key to the order model

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from shop.models import Product
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField
 
 
 class Order(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
