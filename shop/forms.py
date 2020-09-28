bf307f448408e9a40f3be088ee779b62f0be8bcd
harclemadscam <joeapplegate1993@gmail.com>
2020-09-28 20:54:46 +0000

    Created the form to add products to the shop

---
+++ shop/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        category_names = [(c.id, c.name) for c in categories]
+
+        self.fields['category'].choices = category_names
