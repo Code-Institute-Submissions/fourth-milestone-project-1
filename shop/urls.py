0e7a91f5af2efde0d82ed88823428412a4df6b9b
harclemadscam <joeapplegate1993@gmail.com>
2020-09-30 21:26:11 +0000

    Updated shop urls.py with the edit product path

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.shop, name='shop'),
     path('<int:id>/<slug:slug>/', views.product_details, name='product_details'),
     path('add-product/', views.add_new_product, name='add_product'),
+    path('<int:id>/<slug:slug>/edit-product/', views.edit_product, name='edit_product'),
 ]
