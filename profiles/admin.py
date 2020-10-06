1ac1d8f68d4dd0a5731c072ad98ee0b419fc8271
harclemadscam <joeapplegate1993@gmail.com>
2020-10-06 12:42:01 +0000

    Registered user profile admin, added product review and order inlines

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
+from reviews.admin import ProductReviewInline
+from orders.admin import OrderInline
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    inlines = [ProductReviewInline, OrderInline]
+    list_display = ['user']
+    
