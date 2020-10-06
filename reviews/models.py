f4ec15d6f7c12a2bd5176cbf174bb1f782062f7d
harclemadscam <joeapplegate1993@gmail.com>
2020-10-06 12:37:35 +0000

    Created product review model, with user and product as foreign keys

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from shop.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(UserProfile, related_name='user_reviews', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product_reviews', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=False, blank=False)
+    rating = models.PositiveIntegerField(default=1)
+    body = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
