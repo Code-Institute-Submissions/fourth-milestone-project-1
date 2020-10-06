a8d40acd50fe51c0b63e028c3d84847f26796662
harclemadscam <joeapplegate1993@gmail.com>
2020-10-06 10:15:12 +0000

    Set login required decorator for the user profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserOrderDetailsForm
 
 
+@login_required
 def user_profile(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
     form = UserOrderDetailsForm(instance=user_profile)
