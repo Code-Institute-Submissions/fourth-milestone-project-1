8ae3338d949b4623391749ca05b2f557e6c0b1a7
harclemadscam <joeapplegate1993@gmail.com>
2020-10-06 12:38:53 +0000

    Registered the product review model in admin.py, created inline

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ProductReview
 
-# Register your models here.
+
+class ProductReviewInline(admin.TabularInline):
+    model = ProductReview
+    raw_id_fields = ['product', 'user']
+
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'rating']
