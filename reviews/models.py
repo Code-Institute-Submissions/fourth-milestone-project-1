492430ec1006072969d011bbb0300fa9e7493059
harclemadscam <joeapplegate1993@gmail.com>
2020-10-06 16:33:56 +0000

    Set user and product fields on review model to allow blanks

--- reviews/models.py
+++ reviews/models.py
@@ -4,8 +4,8 @@ from profiles.models import UserProfile
 
 
 class ProductReview(models.Model):
-    user = models.ForeignKey(UserProfile, related_name='user_reviews', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='product_reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, related_name='user_reviews', null=True, blank=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product_reviews', null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=False, blank=False)
     rating = models.PositiveIntegerField(default=1)
     body = models.TextField(blank=True)
