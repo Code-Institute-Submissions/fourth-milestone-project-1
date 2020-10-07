e2f7e990cae353e7285a50109d4f9e47226bf03f
harclemadscam <joeapplegate1993@gmail.com>
2020-10-07 13:00:06 +0000

    Created draft view for editing product reviews

--- reviews/views.py
+++ reviews/views.py
@@ -27,3 +27,24 @@ def create_product_review(request, id, slug):
         'form': form,
     }
     return render(request, 'reviews/create_product_review.html', context)
+
+
+@login_required
+def edit_product_review(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    user = UserProfile.objects.get(user=request.user)
+    review = get_object_or_404(ProductReview, user=user, product=product)
+    if request.method == 'POST':
+        form = ProductReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            product.calculate_rating()
+            return redirect(reverse('product_details',
+                            args=[product.id, product.slug]))
+    else:
+        form = ProductReviewForm(instance=review)
+    context = {
+        'product': product,
+        'form': form,
+    }
+    return render(request, 'reviews/edit_product_review.html', context)
