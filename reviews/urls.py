e736efcccf7a1340846f785b614e93062d6e5193
harclemadscam <joeapplegate1993@gmail.com>
2020-10-07 14:15:27 +0000

    Created url for the delete product review view

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:id>/<slug:slug>/create-review/', views.create_product_review, name='create_product_review'),
     path('<int:id>/<slug:slug>/edit-review/', views.edit_product_review, name='edit_product_review'),
+    path('<int:id>/<slug:slug>/delete-review/', views.delete_product_review, name='delete_product_review'),
 ]
