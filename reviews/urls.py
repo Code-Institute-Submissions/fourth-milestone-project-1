28dfb1f8c13373863ccbb6b80e7c7b6c9e8b0378
harclemadscam <joeapplegate1993@gmail.com>
2020-10-07 13:00:51 +0000

    Added url for edit product reviews to urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:id>/<slug:slug>/create-review/', views.create_product_review, name='create_product_review'),
+    path('<int:id>/<slug:slug>/edit-review/', views.edit_product_review, name='edit_product_review'),
 ]
