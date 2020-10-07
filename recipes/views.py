8cfd4c3df58f8baee652881bb289ae814e4a91ea
harclemadscam <joeapplegate1993@gmail.com>
2020-10-07 16:08:25 +0000

    Created draft recipes and recipe details views in recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+
+def recipes(request):
+    recipes = Recipe.objects.all()
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'recipes/recipes.html', context)
+
+
+def recipe_details(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    context = {
+        'recipe': recipe
+    }
+    return render(request, 'recipes/recipe_details.html', context)
