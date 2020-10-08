19556e253b52767772759f6dac2a4fe5e127c771
harclemadscam <joeapplegate1993@gmail.com>
2020-10-08 16:07:53 +0000

    Created view to add recipe in recipes views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
+from .forms import AddRecipeForm
 
 
 def recipes(request):
@@ -16,3 +18,18 @@ def recipe_details(request, id):
         'recipe': recipe
     }
     return render(request, 'recipes/recipe_details.html', context)
+
+
+@login_required
+def add_recipe(request):
+    if request.method == 'POST':
+        form = AddRecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save()
+            return redirect(reverse('recipe_details', args=[recipe.id]))
+    else:
+        form = AddRecipeForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'recipes/add_recipe.html', context)
