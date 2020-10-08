45f3eae9cf34a0762c3ac6a05fa508baa73cdb97
harclemadscam <joeapplegate1993@gmail.com>
2020-10-08 15:28:42 +0000

    Registered the recipe model in django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'score', 'is_approved', 'is_showcased']
+    list_filter = ['is_approved', 'is_showcased']
+    list_editable = ['is_approved', 'is_showcased']
