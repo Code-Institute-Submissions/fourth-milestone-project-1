02b8cc20603cd8730c282eb70c9e72958c90b421
harclemadscam <joeapplegate1993@gmail.com>
2020-10-08 16:08:13 +0000

    Created the add recipe form in recipes app

---
+++ recipes/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Recipe
+
+
+class AddRecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        exclude = ('user', 'score', 'is_showcased', 'is_approved')
