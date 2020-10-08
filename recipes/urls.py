ce876330332b490a2efe01661a7f09b6c8abb8cd
harclemadscam <joeapplegate1993@gmail.com>
2020-10-08 17:33:47 +0000

    Added edit recipe url for recipes app

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.recipes, name='recipes'),
     path('<int:id>/', views.recipe_details, name='recipe_details'),
     path('add-recipe/', views.add_recipe, name='add_recipe'),
+    path('<int:id>/edit-recipe/', views.edit_recipe, name='edit_recipe'),
 ]
