b919ffbae7f7241358d2bd7be4ee7b4e382c9d50
harclemadscam <joeapplegate1993@gmail.com>
2020-10-18 16:58:04 +0000

    Created the view to add a video, based on similar views that add data

--- videos/views.py
+++ videos/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from .models import Video
+from .forms import AddVideoForm
 
 
 def videos(request):
     return render(request, 'videos/videos.html')
+
+
+@login_required
+def add_video(request):
+    if request.method == 'POST':
+        form = AddVideoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('videos'))
+    else:
+        form = AddVideoForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'videos/add_video.html', context)
