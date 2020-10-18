9b00709cfcb78bdd705ed058a8b2cdf30936769d
harclemadscam <joeapplegate1993@gmail.com>
2020-10-18 16:34:12 +0000

    Updated the recipes form to exclude the new saved by users field

--- recipes/forms.py
+++ recipes/forms.py
@@ -5,4 +5,4 @@ from .models import Recipe
 class AddRecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        exclude = ('user', 'score', 'is_showcased', 'is_approved')
+        exclude = ('user', 'score', 'is_showcased', 'is_approved', 'saved_by_users')
