462610d6643752b535aee430cf59f103f61120c7
harclemadscam <joeapplegate1993@gmail.com>
2020-10-18 16:59:00 +0000

    Registered the video model in admin.py

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url', 'is_shown']
+    list_filter = ['is_shown']
+    list_editable = ['is_shown']
