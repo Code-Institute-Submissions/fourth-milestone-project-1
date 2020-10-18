9d1323bb67237a22cf6d047c17c35152e60ca3e0
harclemadscam <joeapplegate1993@gmail.com>
2020-10-18 16:32:55 +0000

    Created the video model for the videos app, made migrations

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    url = models.CharField(max_length=500, db_index=True)
+    is_shown = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
