061c776991c4b14c0cf127b70c5a06d6a091e333
harclemadscam <joeapplegate1993@gmail.com>
2020-10-18 16:32:30 +0000

    Created the galleryimage model for the gallery app, made migrations

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class GalleryImage(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    image = models.ImageField(upload_to='gallery/%Y/%m/%d', blank=False)
+    is_shown = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
