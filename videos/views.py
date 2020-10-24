a9c25efa7dbf1f555cfe6da7aea20ec0bdcbba45
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 13:02:58 +0000

    Created the edit video view in views.py, tweaked videos view

--- videos/views.py
+++ videos/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Video
 from .forms import AddVideoForm
 
 
 def videos(request):
-    return render(request, 'videos/videos.html')
+    videos = Video.objects.all()
+    context = {
+        'videos': videos
+    }
+    return render(request, 'videos/videos.html', context)
 
 
 @login_required
@@ -21,3 +25,20 @@ def add_video(request):
         'form': form,
     }
     return render(request, 'videos/add_video.html', context)
+
+
+@login_required
+def edit_video(request, id):
+    video = get_object_or_404(Video, id=id)
+    if request.method == 'POST':
+        form = AddVideoForm(request.POST, instance=video)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('videos'))
+    else:
+        form = AddVideoForm(instance=video)
+    context = {
+        'form': form,
+        'video': video,
+    }
+    return render(request, 'videos/edit_video.html', context)
