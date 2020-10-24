a6a09ef787c60bf0c4e0f2b1dbf15df1a2b6f388
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 14:27:52 +0000

    Added the delete image path to gallery urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('add-image/', views.add_image, name='add_image'),
     path('<int:id>/edit-image/', views.edit_image, name='edit_image'),
+    path('<int:id>/delete-image/', views.delete_image, name='delete_image'),
 ]
