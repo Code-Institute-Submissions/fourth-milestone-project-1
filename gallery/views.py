3f6900ca876ef31ec654308a8c0bcb774ae11188
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 14:18:55 +0000

    Added the add image view and updated the gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import GalleryImage
+from .forms import UploadImageForm
 
 from shop.models import Product
 
 
 def gallery(request):
     products = Product.objects.all()
+    images = GalleryImage.objects.all()
     context = {
-        'products': products
+        'products': products,
+        'images': images
+
     }
     return render(request, 'gallery/gallery.html', context)
+
+
+@login_required
+def add_image(request):
+    if not request.user.is_superuser:
+        return redirect(reverse('gallery'))
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('gallery'))
+    else:
+        form = UploadImageForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'gallery/add_image.html', context)
