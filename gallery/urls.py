c6490f42dd4a87c948a032f43d3994ab65958e34
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 14:25:16 +0000

    Added the edit image path to urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('add-image/', views.add_image, name='add_image'),
+    path('<int:id>/edit-image/', views.edit_image, name='edit_image'),
 ]
