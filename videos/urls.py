d41a1e1e1ada0f8024db5867ec2c9392b93c8c22
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 13:15:13 +0000

    Added delete video path to videos urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.videos, name='videos'),
     path('add-video/', views.add_video, name='add_video'),
     path('<int:id>/edit-video/', views.edit_video, name='edit_video'),
+    path('<int:id>/delete-video/', views.delete_video, name='delete_video'),
 ]
