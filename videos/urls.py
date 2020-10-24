904661bfd5f6cb0159a1697e62d5a6e700539476
harclemadscam <joeapplegate1993@gmail.com>
2020-10-24 13:03:12 +0000

    Added the edit video path to urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.videos, name='videos'),
     path('add-video/', views.add_video, name='add_video'),
+    path('<int:id>/edit-video/', views.edit_video, name='edit_video'),
 ]
