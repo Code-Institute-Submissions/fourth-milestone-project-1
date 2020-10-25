23a5386e9781a8e95b9c4cf4187f26e718244f54
harclemadscam <joeapplegate1993@gmail.com>
2020-10-25 20:24:26 +0000

    Added the profile recipes path to the profiles urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.user_profile, name='user_profile'),
     path('website-admin/', views.website_admin, name='website_admin'),
+    path('recipes/', views.profile_recipes, name='profile_recipes'),
 ]
