734263a8fafca3c0cf08e149caf1462185ff0581
harclemadscam <joeapplegate1993@gmail.com>
2020-10-25 17:36:55 +0000

    Updated recipes urls.py with save recipe path

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-recipe/', views.add_recipe, name='add_recipe'),
     path('<int:id>/edit-recipe/', views.edit_recipe, name='edit_recipe'),
     path('<int:id>/delete-recipe/', views.delete_recipe, name='delete_recipe'),
+    path('<int:id>/save-recipe/', views.save_recipe, name='save_recipe'),
 ]
