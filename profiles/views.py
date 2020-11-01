e65e2575962b809d6d543273daef6836f49e0c82
harclemadscam <joeapplegate1993@gmail.com>
2020-11-01 19:28:57 +0000

    Updated profile recipes view with pagination for saved and user recipes

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import UserProfile
 from .forms import UserOrderDetailsForm
@@ -32,7 +33,23 @@ def website_admin(request):
 def profile_recipes(request):
     user_profile = get_object_or_404(UserProfile, user=request.user)
     user_recipes = Recipe.objects.filter(user=user_profile)
-    saved_recipes = Recipe.objects.filter(saved_by_users=user_profile)
+    saved_recipes = Recipe.objects.filter(saved_by_users=user_profile, is_approved=True)
+    paginator = Paginator(user_recipes, 1)
+    page = request.GET.get('page1')
+    try:
+        user_recipes = paginator.page(page)
+    except PageNotAnInteger:
+        user_recipes = paginator.page(1)
+    except EmptyPage:
+        user_recipes = paginator.page(paginator.num_pages)
+    paginator = Paginator(saved_recipes, 1)
+    page = request.GET.get('page2')
+    try:
+        saved_recipes = paginator.page(page)
+    except PageNotAnInteger:
+        saved_recipes = paginator.page(1)
+    except EmptyPage:
+        saved_recipes = paginator.page(paginator.num_pages)
     context = {
         'user_profile': user_profile,
         'user_recipes': user_recipes,
