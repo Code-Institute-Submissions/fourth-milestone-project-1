9c709f80bae34c31797b532d9f4d9c07a047e805
harclemadscam <joeapplegate1993@gmail.com>
2020-11-13 18:58:01 +0000

    Added jquery to hide and show product description when button is clicked

--- shop/static/shop/js/shop.js
+++ shop/static/shop/js/shop.js
@@ -1,3 +1,13 @@
 $(".card").hover( function() {
     $(this).find(".product-card-form").toggleClass("d-none")
 });
+
+$("#read-more").click( function() {
+    $("#product-description-truncated").toggleClass("d-none")
+    $("#product-description-full").toggleClass("d-none")
+})
+
+$("#read-less").click( function() {
+    $("#product-description-full").toggleClass("d-none")
+    $("#product-description-truncated").toggleClass("d-none")
+})
