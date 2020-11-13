5b45dd44e74ab520da1f1f2db5312822c6e1c76a
harclemadscam <joeapplegate1993@gmail.com>
2020-11-13 17:40:09 +0000

    Updated scripts.js to add and remove header container classes on scroll

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,9 @@
 $(window).scroll(function() {
     if ($(window).scrollTop() > 50) {
         $("#brand").addClass("scroll")
+        $("#header-container").addClass("header-container-scroll").removeClass("header-container-normal")
     } else {
         $("#brand").removeClass("scroll")
+        $("#header-container").addClass("header-container-normal").removeClass("header-container-scroll")
     }
 });
