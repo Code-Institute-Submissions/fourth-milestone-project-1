7dc83a640efe490d05b7bac62b6037d60739db89
harclemadscam <joeapplegate1993@gmail.com>
2020-11-13 19:48:20 +0000

    Added jquery to show loading spinner button when submit btn clicked

--- shop/static/shop/js/shop.js
+++ shop/static/shop/js/shop.js
@@ -5,9 +5,14 @@ $(".card").hover( function() {
 $("#read-more").click( function() {
     $("#product-description-truncated").toggleClass("d-none")
     $("#product-description-full").toggleClass("d-none")
-})
+});
 
 $("#read-less").click( function() {
     $("#product-description-full").toggleClass("d-none")
     $("#product-description-truncated").toggleClass("d-none")
-})
+});
+
+$("#product-details-submit").click( function() {
+    $("#product-details-submit").toggleClass("d-none")
+    $("#product-details-loading").toggleClass("d-none")
+});
