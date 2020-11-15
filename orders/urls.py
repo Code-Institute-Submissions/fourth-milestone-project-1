6a820da3b5dd86791a8d47f5da9c6a61ab67035b
harclemadscam <joeapplegate1993@gmail.com>
2020-11-15 00:02:41 +0000

    Updated the orders urls.py with the order complete path

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .webhooks import webhook
 
 urlpatterns = [
-    path('create/', views.create_order, name='create_order'),
+    path('checkout/', views.create_order, name='create_order'),
+    path('complete/<id>/', views.order_complete, name='order_complete'),
     path('wh/', webhook, name='webhook'),
 ]
