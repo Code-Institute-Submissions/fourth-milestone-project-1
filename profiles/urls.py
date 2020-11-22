0636654b484478c7bd93daf82badfdf0c4f6da01
harclemadscam <joeapplegate1993@gmail.com>
2020-11-22 15:18:34 +0000

    Added path to user orders in profiles app urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user_profile, name='user_profile'),
+    path('orders/', views.user_orders, name='user_orders'),
     path('website-admin/', views.website_admin, name='website_admin'),
     path('my-recipes/', views.user_recipes, name='user_recipes'),
     path('saved-recipes/', views.saved_recipes, name='saved_recipes'),
