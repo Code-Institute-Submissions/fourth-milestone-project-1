58f9b31a4e570c3ce86a3f5215281b06d0fb4d00
harclemadscam <joeapplegate1993@gmail.com>
2020-11-22 20:34:07 +0000

    Updated create order view to update user defaults if checkbox checked

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,7 @@ from cart.cart import Cart
 from django.conf import settings
 
 from profiles.models import UserProfile
+from profiles.forms import UserOrderDetailsForm
 
 import stripe
 
@@ -30,10 +31,21 @@ def create_order(request):
                 user_profile = UserProfile.objects.get(user=request.user)
                 order.user_profile = user_profile
                 order.save()
+                update_default = 'update-default' in request.POST
+                if update_default:
+                    new_default = {
+                        'default_address_line1': order.address_line1,
+                        'default_address_line2': order.address_line2,
+                        'default_town_or_city': order.town_or_city,
+                        'default_postcode': order.postcode,
+                        'default_country': order.country,
+                    }
+                    form = UserOrderDetailsForm(new_default, instance=user_profile)
+                    if form.is_valid():
+                        form.save()
             cart.clear()
             return redirect(reverse('order_complete', args=[order.id]))
     else:
-        
         total = cart.get_total_price()
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
@@ -55,7 +67,7 @@ def create_order(request):
             except UserProfile.DoesNotExist:
                 form = CreateOrderForm()
         else:
-            form = CreateOrderForm()       
+            form = CreateOrderForm()
         context = {
             'cart': cart,
             'form': form,
