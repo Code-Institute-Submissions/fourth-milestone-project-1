d41f320808650b4ab11b706a7b81cdafe18c5925
harclemadscam <joeapplegate1993@gmail.com>
2020-11-24 22:29:37 +0000

    Added rating choices to rating in product review model

--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,15 @@ class ProductReview(models.Model):
     user = models.ForeignKey(UserProfile, related_name='user_reviews', null=True, blank=True, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='product_reviews', null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=False, blank=False)
-    rating = models.PositiveIntegerField(default=1)
+    RATING_CHOICES = [
+        (0, '0 Stars'),
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    ]
+    rating = models.PositiveIntegerField(default=1, choices=RATING_CHOICES)
     body = models.TextField(blank=True)
 
     def __str__(self):
