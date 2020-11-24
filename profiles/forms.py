8a4b6a649184d15236a9647b1981a7734329202a
harclemadscam <joeapplegate1993@gmail.com>
2020-11-24 22:04:38 +0000

    Updated profile page default delivery form labels to be friendlier

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,3 +6,10 @@ class UserOrderDetailsForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         exclude = ('user',)
+        labels = {
+            'default_address_line1': 'Address Line 1',
+            'default_address_line2': 'Address Line 2',
+            'default_town_or_city': 'Town or City',
+            'default_postcode': 'Postcode',
+            'default_country': 'Country',
+        }
