b5a135e3649e913a5f9081cc2fd7cd5dc54875c9
harclemadscam <joeapplegate1993@gmail.com>
2020-11-29 11:42:55 +0000

    Updated settings.py to remove secret key and set debug based on environ

--- miss_pans_bakery/settings.py
+++ miss_pans_bakery/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b+2w6xgk-!cvg6dnt1_+c*%c$+1&2+ky@p=)nf++zqx9!l6lan'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['miss-pans-bakery.herokuapp.com', 'localhost']
 
